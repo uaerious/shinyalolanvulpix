2499cf0bb612689f520bc47a8f517573b1c54c12
uaerious <84714801+uaerious@users.noreply.github.com>
2021-07-19 20:34:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 from datetime import date
 
 today_date = date.today()
@@ -7,7 +6,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from uuid import uuid4
 
-updater = Updater(token='1770413790:AAHzEfkjLUlcq2OH5cAAml_AWV7-t9gir6A', use_context=True)
+updater = Updater(token='TOKEN', use_context=True)
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -17,7 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # 1
 def start(update, context):
-    # echo off by default
+    # echo off by default; sends alot of internal error
     update.message.reply_text(f'Welcome to this bot\n'
                               f'/help for assistance 😉')
 
@@ -27,7 +26,7 @@ def error(update, context):
     update.message.reply_text(f'This are current errors :o\n\n'
                               f'Error list:\n\n'
                               f'Error 1: Not a valid command\n'
-                              f'Error 2: Currently under maintenance'
+                              f'Error 2: Currently under maintenance...'
                               )
 
 
@@ -36,9 +35,10 @@ def help(update, context):
     update.message.reply_text(f'/start to ensure bot is up to date\n\n'
                               f'/hello for a warm greeting 😃\n\n'
                               f'/feelings to send your feelings 🥰\n\n'
+                              f'/goodmorning :D morning!\n\n'
+                              f'/goodnight D: goodnight!\n\n'
                               f'/error to look at known error list\n\n'
                               f'/recent for recent update notes\n\n'
-                              f'/QOTD for quote of the day!\n\n'
                               f'/echooff turns off echo\n\n'
                               f'/echoon turns on echo - currently OFF by default\n'
                               f'go rawr at the bot with echo hehe\n\n'
@@ -58,36 +58,25 @@ def hello(update, context) -> None:
 
 # 5
 def feelings(update, context):
-    update.message.reply_text(f'How are you feeling today?')  # need ReplyKeyboardMarkup
+    update.message.reply_text(f'How are you feeling today?')  # need inlinekeyboardMarkup
 
 
 # 6
 def recent(update, context):
-    update.message.reply_text(f'Last update: 7/7/2021\n\n'
-                              f'-New commands hehe\n'
-                              f'-simpler codes whee\n'
-                              f'-echo is OFF by DEFAULT and can be turned ON\n'
+    update.message.reply_text(f'Last update: 19/7/2021\n\n'
                               f'-working hard to bring in InlineKeyboard\n'
-                              f'-making the bot more friendly\n'
-                              f'-some features work now...\n'
-                              f'soon to be in beta...'
+                              f'-removed QOTD as I cant find a QOTD website :(\n'
+                              f'good morning! or good night! 😉'
                               )
 
 
 # 7
 def secret(update, context):
     update.message.reply_text(f'WOAH!\n'
-                              f'https://open.spotify.com/playlist/429y59z0RjyDszbJbGfn7m?si=eb3a48e033504782')
-
+                              f'https://open.spotify.com/playlist/429y59z0RjyDszbJbGfn7m?si=eb3a48e033504782\n\n'
+                              f'https://open.spotify.com/track/50BmNitVXwo8yi2VieV1ME?si=6b694677609d433d')
 
 # 8
-def QOTD(update, context):
-    update.message.reply_text(f'https://www.brainyquote.com/topics/daily-quotes\n'
-                              f'is the link ok? or should we just have the quote itself?\n'
-                              f'tell wayn hehe i cant collect responses yet D:')
-
-
-# 9 
 def ily(update, context):
     update.message.reply_text(f'ily🥺🥰\n'
                               f'https://www.youtube.com/watch?v=T_VJv_079l8\n\n'
@@ -95,12 +84,12 @@ def ily(update, context):
                               f'https://www.youtube.com/watch?v=zb_IOQhHvsE')
 
 
-# 10 InlineKeyboard set status?
+# 9 InlineKeyboard set status?
 def status(update, context):
     update.message.reply_text(f'ight what now')
 
 
-# 11 very lengthy echo setup :rolleye:
+# 10 very lengthy echo setup :rolleye:
 
 def put(update, context):
     """Usage: /put value"""
@@ -149,54 +138,58 @@ def echooff(update, context):
     context.user_data['is_echo'] = False
     update.message.reply_text(f'turned off now...')
 
-
-# 12 inlinekeyboard for the user to pick a choice
-def sps(update, context):
-    possible_actions = ['scissors', 'paper', 'stone']
-    computer_action = random.choice(possible_actions)
-    user_choice = ['scissors', 'paper', 'stone']
-    update.message.reply_text(f'I chose {computer_action}, you chose {user_choice}!')
-
-
-# 13 downtime
+# 11
 def downtime(update, context):
     update.message.reply_text(f'there is no maintenance currently...\n'
                               f'next scheduled update: NA')
 
 
-# 14 version
+# 12
 def version(update, context):
-    update.message.reply_text(f'CLOSED TESTING ALPHA\n'
+    update.message.reply_text(f'OPEN TESTING ALPHA\n'
                               f'Version: Alpha\n\n'
                               f'check /recent for the latest update')
 
 
+# 13
+def goodmorning(update, context):
+    update.message.reply_text(f'good morning,{update.effective_user.first_name}! 🥺')
+
+
+# 14
+def goodnight(update, context):
+    update.message.reply_text(f'good night,{update.effective_user.first_name}! 🥺\n'
+                              f'time for me to sleep toooo...')
+
+
 # 15 april fools stuff
 def update_bot(update, context):
-    update.message.reply_text(f'HAH U LEGIT THOUGHT THERE WAS?'
-                              f'APRIL FOOLS')
+    update.message.reply_text(f'HAH U LEGIT THOUGHT THERE WAS?\n'
+                              f'APRIL FOOLS!')
 
 
 # to start the commands with handler and dispatcher
 
-dispatcher = updater.dispatcher
+dp = updater.dispatcher
 
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('error', error))
-dispatcher.add_handler(CommandHandler('help', help))
-dispatcher.add_handler(CommandHandler('hello', hello))
-dispatcher.add_handler(CommandHandler('feelings', feelings))
-dispatcher.add_handler(CommandHandler('recent', recent))
-dispatcher.add_handler(CommandHandler('secret', secret))
-dispatcher.add_handler(CommandHandler('QOTD', QOTD))
-dispatcher.add_handler(CommandHandler('ily', ily))
-dispatcher.add_handler(CommandHandler('status', status))
-dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-dispatcher.add_handler(CommandHandler('echooff', echooff))
-dispatcher.add_handler(CommandHandler('echoon', echoon))
-dispatcher.add_handler(CommandHandler('downtime', downtime))
-dispatcher.add_handler(CommandHandler('version', version))
-dispatcher.add_handler(CommandHandler('update_bot',update_bot))
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(CommandHandler('error', error))
+dp.add_handler(CommandHandler('help', help))
+dp.add_handler(CommandHandler('hello', hello))
+dp.add_handler(CommandHandler('feelings', feelings))
+dp.add_handler(CommandHandler('recent', recent))
+dp.add_handler(CommandHandler('secret', secret))
+dp.add_handler(CommandHandler('ily', ily))
+dp.add_handler(CommandHandler('status', status))
+dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+dp.add_handler(CommandHandler('echooff', echooff))
+dp.add_handler(CommandHandler('echoon', echoon))
+dp.add_handler(CommandHandler('downtime', downtime))
+dp.add_handler(CommandHandler('version', version))
+dp.add_handler(CommandHandler('goodmorning', goodmorning))
+dp.add_handler(CommandHandler('goodnight', goodnight))
+dp.add_handler(CommandHandler('update_bot',update_bot))
+
 
 # when user sends commands that are not added
 def unknown(update, context):
@@ -205,7 +198,7 @@ def unknown(update, context):
                               f"/error for more details :D")
 
 
-dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+dp.add_handler(MessageHandler(Filters.command, unknown))
 
 # waiting for commands from user
 updater.start_polling()
